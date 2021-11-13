48a42c5b1dcd7aeedfed96c7839b90b4fac345fd
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2021-11-13 16:07:16 -0700

    Replaced test with jest test

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,17 @@
 import { describe, it, expect } from "@jest/globals";
+import { Calculator, Input } from ".";
 
 describe("Calculator", () => {
-  it("should do something", () => {});
+  it("should show player 1 beating player 2 by a lot", () => {
+    const firstHand = "Ac,Ad";
+    const secondHand = "7c,2d";
+    const input: Input = {
+      hands: [firstHand, secondHand],
+      numPlayers: 2,
+      iterations: 10000,
+    };
+    const calculator = new Calculator(input);
+    const results = calculator.simulate();
+    expect(results[firstHand].winPercent).toBeGreaterThan(70);
+  });
 });
